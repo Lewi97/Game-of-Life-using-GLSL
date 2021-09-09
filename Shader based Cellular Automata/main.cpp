ed277384efbbb112c9210aa3cd4e27c9c18741a9
Lewi <privkoen00@gmail.com>
2021-09-09 20:26:18 +0200

    Add project files.

---
+++ Shader based Cellular Automata/main.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+#include <raylib.h>
+#include <string>
+
+void fillScreen(Vector2 screen, RenderTexture2D& target)
+{
+    BeginTextureMode(target);
+
+    for (int x = 0; x < screen.x; x++)
+        for (int y = 0; y < screen.y; y++)
+            DrawPixel(x, y, BLACK);
+
+    EndTextureMode();
+}
+
+void fillScreenRand(Vector2 screen, RenderTexture2D& target)
+{
+    BeginTextureMode(target);
+
+    for (int x = 0; x < screen.x; x++)
+        for (int y = 0; y < screen.y; y++)
+            if (GetRandomValue(1, 2) & 1)
+                DrawPixel(x, y, BLACK);
+            else
+                DrawPixel(x, y, WHITE);
+
+    EndTextureMode();
+}
+
+void swapPointers(void* p1, void* p2)
+{
+    void* swap = p1;
+    p1 = p2;
+    p2 = p1;
+}
+
+void makeVerticalLine(int width, int height, RenderTexture2D& target)
+{
+    BeginTextureMode(target);
+    for (int y = 0; y < height; y++)
+        DrawPixel(width, y, WHITE);
+
+    EndTextureMode();
+}
+
+int main()
+{
+    const int screenWidth = 1280;
+    const int screenHeight = 720;
+
+    InitWindow(screenWidth, screenHeight, "Raylib GLSL GoL");
+    
+    RenderTexture2D currentGen = LoadRenderTexture(screenWidth, screenHeight);
+    RenderTexture2D nextGen = LoadRenderTexture(screenWidth, screenHeight);
+
+    Rectangle targetRect{ 0, 0, (float)currentGen.texture.width, (float)-currentGen.texture.height };
+
+    Shader shader = LoadShader(0, "shaders/gol.frag");
+    int u_resolution_loc = GetShaderLocation(shader, "u_resolution");
+    int u_cur_loc = GetShaderLocation(shader, "u_cur");
+
+    float s_resolution[2] = { (float)screenWidth, (float)screenHeight };
+    
+    SetShaderValueTexture(shader, u_cur_loc, currentGen.texture);
+    SetShaderValue(shader, u_resolution_loc, s_resolution, SHADER_UNIFORM_VEC2);
+
+    bool runSimulation = false;
+    float simTimer = 0.f;
+    float simUpdateTime = 0.016f;
+
+    fillScreen({ (float)screenWidth, (float)screenHeight }, nextGen);
+    fillScreenRand({ (float)screenWidth, (float)screenHeight }, nextGen);
+    
+    currentGen.texture = nextGen.texture;
+
+    while (!WindowShouldClose())
+    {
+        const float delta = GetFrameTime();
+
+        if (IsMouseButtonDown(MOUSE_LEFT_BUTTON))
+        {
+            BeginTextureMode(nextGen);
+                Vector2 mouse = GetMousePosition();
+                DrawPixelV(mouse, WHITE);
+            EndTextureMode();
+        }
+
+        if (IsKeyPressed(KEY_SPACE))
+            runSimulation = !runSimulation;
+
+        if (IsKeyPressed(KEY_C))
+            fillScreen({screenWidth, screenHeight}, nextGen);
+
+        SetShaderValueTexture(shader, u_cur_loc, currentGen.texture);
+
+        if (IsKeyPressed(KEY_A))
+            makeVerticalLine(GetMouseX(), screenHeight, nextGen);
+
+        if (runSimulation)
+        {
+            simTimer += delta;
+            if (simTimer > simUpdateTime)
+            {
+                swapPointers(&currentGen, &nextGen);
+                BeginTextureMode(nextGen);
+                    BeginShaderMode(shader);
+                        DrawTextureRec(nextGen.texture, targetRect, { 0.f,0.f }, WHITE);
+                    EndShaderMode();
+                EndTextureMode();
+                simTimer = 0.f;
+            }
+        }
+
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+            DrawTextureRec(nextGen.texture, targetRect, { 0.f,0.f }, WHITE);
+            DrawFPS(10, 10);
+            std::string infoString = "Cells: " + std::to_string(screenHeight * screenWidth);
+            DrawText(infoString.c_str(),10,30,20,RED);
+
+        EndDrawing();
+
+    }
+
+    CloseWindow();
+
+    return 0;
+}
---
+++ Shader based Cellular Automata/shaders/gol.frag
@@ -0,0 +1,54 @@
+#version 330
+
+precision highp float;
+
+uniform vec2 u_resolution;
+
+uniform sampler2D u_cur;
+
+int isPixelOn(vec2 pixOffset)
+{
+    return int(ceil(texture2D(u_cur, (gl_FragCoord.xy + pixOffset) / u_resolution).r));
+}
+
+void main(){
+    vec2 p = gl_FragCoord.xy / u_resolution;
+
+    vec2 rules[9] = vec2[9]
+    (
+        vec2(0,0), // 0 x = s / y = b
+        vec2(0,0), // 1
+        vec2(1,0), // 2
+        vec2(0,1), // 3
+        vec2(0,0), // 4
+        vec2(0,0), // 5
+        vec2(0,0), // 6
+        vec2(0,0), // 7
+        vec2(0,0)  // 8
+    );
+
+    int sum = 
+        isPixelOn( vec2(1,0)  ) + // left
+        isPixelOn( vec2(-1,0) ) + // right
+        isPixelOn( vec2(0,1)  ) + // top
+        isPixelOn( vec2(0,-1) ) + // bottom
+
+        isPixelOn( vec2(1,1)  ) + // top left
+        isPixelOn( vec2(1,-1) ) + // bot left
+        isPixelOn( vec2(-1,1) ) + // top right
+        isPixelOn( vec2(-1,-1));  // bot right
+
+    vec4 image = texture2D(u_cur, p);
+
+    float color = rules[sum].x * image.r + rules[sum].y;
+    
+    if (color > 0.)
+        image.rgb = vec3(color);
+    else
+    {
+        image.r = 0.;
+        image.gb -= vec2(0.01, 0.04);
+    }
+    
+    gl_FragColor = vec4(image);
+}
