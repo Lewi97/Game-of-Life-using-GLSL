917e7efcbbe46844351fb58040740517ca75522a
Lewi <privkoen00@gmail.com>
2021-09-30 19:02:53 +0200

    added mouse movement controls for zooming and dragging

--- Shader based Cellular Automata/main.cpp
+++ Shader based Cellular Automata/main.cpp
@@ -39,7 +39,16 @@ void makeVerticalLine(int width, int height, RenderTexture2D& target)
 {
     BeginTextureMode(target);
 
-        DrawRectangle(width, 50, 1, height-100, WHITE);
+        DrawRectangle(width, 0, 1, height, WHITE);
+
+    EndTextureMode();
+}
+
+void makeHorizontalLine(int width, int height, RenderTexture2D& target)
+{
+    BeginTextureMode(target);
+
+    DrawRectangle(0, height, width, 1, WHITE);
 
     EndTextureMode();
 }
@@ -49,20 +58,24 @@ int main()
     const int screenWidth = 1280;
     const int screenHeight = 720;
 
+    const int canvasWidth = screenWidth;
+    const int canvasHeight = screenHeight;
+
     InitWindow(screenWidth, screenHeight, "Raylib GLSL GoL");
     
-    RenderTexture2D currentGen = LoadRenderTexture(screenWidth, screenHeight);
-    RenderTexture2D nextGen = LoadRenderTexture(screenWidth, screenHeight);
-
+    RenderTexture2D currentGen =    LoadRenderTexture(canvasWidth, canvasHeight);
+    RenderTexture2D nextGen =       LoadRenderTexture(canvasWidth, canvasHeight);
+    RenderTexture2D target =        LoadRenderTexture(canvasWidth, canvasHeight);
+    
     auto* currentPointer = &currentGen;
     auto* nextPointer = &nextGen;
 
-    Rectangle targetRect{ 0, 0, (float)currentGen.texture.width, (float)-currentGen.texture.height };
+    Rectangle shaderRect{ 0, 0, (float)currentGen.texture.width, (float)-currentGen.texture.height };
 
     Shader shader = LoadShader(0, "shaders/gol.frag");
     int u_resolution_loc = GetShaderLocation(shader, "u_resolution");
     int u_cur_loc = GetShaderLocation(shader, "u_cur");
-    float s_resolution[2] = { (float)screenWidth, (float)screenHeight };
+    float s_resolution[2] = { (float)target.texture.width, (float)target.texture.height };
     
     SetShaderValue(shader, u_resolution_loc, s_resolution, SHADER_UNIFORM_VEC2);
 
@@ -72,26 +85,55 @@ int main()
 
     fillScreen({ (float)screenWidth, (float)screenHeight }, nextGen);
     fillScreen({ (float)screenWidth, (float)screenHeight }, currentGen);
+    //fillScreenRand({ (float)screenWidth, (float)screenHeight }, currentGen);
 
-    SetShaderValueTexture(shader, u_cur_loc, currentGen.texture);
+    SetShaderValueTexture(shader, u_cur_loc, nextGen.texture);
+
+    Camera2D camera{ {target.texture.width / 2.f, target.texture.height / 2.f}, {target.texture.width / 2.f, target.texture.height / 2.f}, 0.f,1.f };
+
+    std::string infoString = "Cells: " + std::to_string(target.texture.width * target.texture.height);
+
+    BeginTextureMode(target);
+    EndTextureMode();
+    SetShaderValueTexture(shader, u_cur_loc, currentPointer->texture);
+    BeginDrawing();
+    DrawText(infoString.c_str(), 10, 30, 20, RED);
+    EndDrawing();
+
+    Vector2 oldMousePos{};
 
     while (!WindowShouldClose())
     {
         const float delta = GetFrameTime();
+        Vector2 mouse = GetMousePosition();
 
         if (IsMouseButtonDown(MOUSE_LEFT_BUTTON))
         {
             BeginTextureMode(*currentPointer);
-                Vector2 mouse = GetMousePosition();
-                DrawPixelV(mouse, WHITE);
+            Vector2 worldMouse = GetScreenToWorld2D(mouse, camera);
+            DrawPixelV(worldMouse, WHITE);
             EndTextureMode();
         }
+
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+            std::cout << "Current " << currentPointer->id << " Next " << nextPointer->id << std::endl;
+
+        if (IsMouseButtonDown(MOUSE_MIDDLE_BUTTON))
+        {
+            camera.target.x += oldMousePos.x - mouse.x;
+            camera.target.y += oldMousePos.y - mouse.y;
+        }
+
+        camera.zoom += ((float)GetMouseWheelMove() * 0.05f);
 
         if (IsKeyPressed(KEY_SPACE))
             runSimulation = !runSimulation;
 
         if (IsKeyPressed(KEY_C))
-            fillScreen({screenWidth, screenHeight}, *currentPointer);
+            fillScreen({ (float)currentGen.texture.width, (float)currentGen.texture.height }, *currentPointer);
+
+        if (IsKeyPressed(KEY_D))
+            makeHorizontalLine(screenWidth, GetMouseY(), *currentPointer);
 
         if (IsKeyPressed(KEY_A))
             makeVerticalLine(GetMouseX(), screenHeight, *currentPointer);
@@ -99,13 +141,19 @@ int main()
         if (IsKeyPressed(KEY_B))
             spawnGlider(GetMouseX(), GetMouseY(), *currentPointer);
 
+        BeginTextureMode(target);
+        ClearBackground(RAYWHITE);
+        DrawTexture(currentPointer->texture, 0,0, WHITE);
+
+        EndTextureMode();
+
         simTimer += delta;
         if (runSimulation)
             if (simTimer > simUpdateTime)
             {
                 BeginTextureMode(*nextPointer);
                     BeginShaderMode(shader);
-                        DrawTextureRec(nextPointer->texture, targetRect, { 0.f,0.f }, WHITE);
+                    DrawTextureRec(nextPointer->texture, shaderRect, {0.f,0.f}, WHITE);
                     EndShaderMode();
                 EndTextureMode();
                 
@@ -117,17 +165,20 @@ int main()
                 
                 SetShaderValueTexture(shader, u_cur_loc, currentPointer->texture);
             }
-        
 
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
-            DrawTextureRec(currentPointer->texture, targetRect, { 0.f,0.f }, WHITE);
+
+            BeginMode2D(camera);
+            DrawTextureRec(target.texture, { 0.f,0.f,(float)target.texture.width,(float)target.texture.height }, { 0.f,0.f }, WHITE);
+            EndMode2D();
             DrawFPS(10, 10);
-            std::string infoString = "Cells: " + std::to_string(screenHeight * screenWidth);
             DrawText(infoString.c_str(),10,30,20,RED);
 
         EndDrawing();
+
+        oldMousePos = mouse;
     }
 
     UnloadRenderTexture(currentGen);
